c5f0bc23588c4da9f26363aa66022d1455f59947
crscillitoe <crscillitoe@wpi.edu>
2018-12-19 19:56:28 -0500

    initial push of timer service

---
+++ front-end/src/app/services/timer/timer.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TimerService {
+  /*
+   *
+   */
+  constructor() { }
+
+  /*
+   * 1 - Success
+   * 0 - Failure
+   */
+  int startTimer() { }
+  int pauseTimer() { }
+  int stopTimer() { }
+}
