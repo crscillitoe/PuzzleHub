7a056ee930ec7db06b22673b0874836e88863630
crscillitoe <crscillitoe@wpi.edu>
2018-12-23 23:47:51 -0500

    front-end header created, routes configured

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -2,18 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
 import { HeaderInterceptorService } from './services/header-interceptor/header-interceptor.service';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+
+const appRoutes: Routes = [
+    { path: '', component: MainMenuComponent},
+    { path: 'login', component: LoginComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainMenuComponent
+    MainMenuComponent,
+    HeaderComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(
+        appRoutes,
+        { enableTracing: false }
+    )
   ],
   providers: [
     {
