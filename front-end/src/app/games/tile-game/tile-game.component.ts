b2d6358e5648403250d7e347b0325798baa840f6
crscillitoe <crscillitoe@wpi.edu>
2018-12-27 15:52:08 -0500

    tile game initial working version

--- front-end/src/app/games/tile-game/tile-game.component.ts
+++ front-end/src/app/games/tile-game/tile-game.component.ts
@@ -19,13 +19,20 @@ export class TileGameComponent implements OnInit {
   canvas: any;
   context: any;
 
+
+  gridBoxSize: number;
   colors: any;
   
   canvasOffsetX: number = 225;
   canvasOffsetY: number = 56;
 
+  gridOffsetX: number = 100;
+  gridOffsetY: number = 100;
+
   difficulty: number;
   seed: number;
+
+  board: Board;
 
   constructor(
     private route: ActivatedRoute, 
@@ -43,24 +50,35 @@ export class TileGameComponent implements OnInit {
     this.canvas = document.getElementById('myCanvas');
     this.context = this.canvas.getContext('2d');
 
+    var width;
+    var height;
+
     // Easy
     if(this.difficulty == 1) {
       console.log('Easy difficulty');
+      width = 3;
+      height = 3;
     } 
     
     // Medium
     else if (this.difficulty == 2) {
       console.log('Medium difficulty');
+      width = 4;
+      height = 4;
     } 
     
     // Hard
     else if (this.difficulty == 3) {
       console.log('Hard difficulty');
+      width = 5;
+      height = 5;
     } 
     
     // Extreme
     else if (this.difficulty == 4) {
       console.log('Extreme difficulty');
+      width = 6;
+      height= 6;
     }
 
     // Uncomment these to add event listeners
@@ -68,7 +86,7 @@ export class TileGameComponent implements OnInit {
     //this.canvas.addEventListener('mouseup',   (e) => this.mouseReleased(e), false);
     //this.canvas.addEventListener('mousemove', (e) => this.mouseMove(e),     false);
 
-    //window.addEventListener('keydown', (e) => this.keyPressed(e),  false);
+    window.addEventListener('keydown', (e) => this.keyPressed(e),  false);
     //window.addEventListener('keyup',   (e) => this.keyReleased(e), false);
 
 
@@ -79,12 +97,22 @@ export class TileGameComponent implements OnInit {
           // Generate board with given seed
           this.seed = data['seed'];
 
+          this.board = new Board(width, height, this.seed); 
+          this.board.generateBoard();
+
+          this.fixSizes();
           this.draw();
         });
     } else {
       // Generate board with random seed
       this.seed = Math.floor(Math.random() * (2000000000));
 
+      this.board = new Board(width, height, this.seed); 
+      this.board.generateBoard();
+
+      console.log(this.board.tilePuzzle);
+
+      this.fixSizes();
       this.draw();
     }
   }
@@ -92,12 +120,104 @@ export class TileGameComponent implements OnInit {
   draw() {
     this.context.beginPath();
     this.drawBackground();
+    this.drawGrid();
+    this.drawTiles();
   }
 
   drawBackground() {
     this.context.fillStyle = this.colors.BACKGROUND; // Background color
     this.context.fillRect(0, 0, this.canvas.offsetWidth * 2, this.canvas.offsetHeight * 2);
   }
+
+  drawGrid() {
+    for(var i = 0 ; i <= this.board.width ; i++) {
+      this.context.lineWidth = 1;
+      this.context.strokeStyle = this.colors.COLOR_1;
+      this.context.moveTo(this.gridOffsetX + (i * this.gridBoxSize), this.gridOffsetY);
+      this.context.lineTo(this.gridOffsetX + (i * this.gridBoxSize), this.gridOffsetY + (this.board.height * this.gridBoxSize));
+      this.context.stroke();
+    }
+
+    for(var j = 0 ; j <= this.board.height ; j++) {
+      this.context.lineWidth = 1;
+      this.context.strokeStyle = this.colors.FOREGROUND;
+      this.context.moveTo(this.gridOffsetX,                                         this.gridOffsetY + (j * this.gridBoxSize));
+      this.context.lineTo(this.gridOffsetX + (this.board.width * this.gridBoxSize), this.gridOffsetY + (j * this.gridBoxSize));
+      this.context.stroke();
+    }
+  }
+
+  drawTiles() {
+    for(var j = 0 ; j < this.board.height ; j++) {
+      for(var i = 0 ; i < this.board.width; i++) {
+
+        var boardValue = this.board.tilePuzzle[j][i];
+        var tileString = "" + boardValue;
+        if(tileString == '0') tileString = '';
+
+        this.context.font = 'bold ' + Math.floor(this.gridBoxSize / 1.4) + 'px Arial';
+
+        switch(boardValue % 8) {
+          case(0):
+            this.context.fillStyle = this.colors.COLOR_7;
+            break;
+          case(1):
+            this.context.fillStyle = this.colors.COLOR_0;
+            break;
+          case(2):
+            this.context.fillStyle = this.colors.COLOR_1;
+            break;
+          case(3):
+            this.context.fillStyle = this.colors.COLOR_2;
+            break;
+          case(4):
+            this.context.fillStyle = this.colors.COLOR_3;
+            break;
+          case(5):
+            this.context.fillStyle = this.colors.COLOR_4;
+            break;
+          case(6):
+            this.context.fillStyle = this.colors.COLOR_5;
+            break;
+          case(7):
+            this.context.fillStyle = this.colors.COLOR_6;
+            break;
+        }
+
+        if(tileString.length == 2) {
+          this.context.fillText(tileString, (this.gridOffsetX) + ( i * this.gridBoxSize ) + (this.gridBoxSize / 8),
+                                            (this.gridOffsetY) + ( (j + 1) * this.gridBoxSize ) - (this.gridBoxSize / 4));
+        } else {
+          this.context.fillText(tileString, (this.gridOffsetX) + ( i * this.gridBoxSize ) + (this.gridBoxSize / 3.5),
+                                            (this.gridOffsetY) + ( (j + 1) * this.gridBoxSize ) - (this.gridBoxSize / 4));
+        }
+      }
+    }
+  }
+
+  /* Line
+    context.lineWidth = 1;
+    context.strokeStyle = that.gridColor;
+    context.moveTo(circleX, circleY);
+    context.lineTo(circleX, circleY2);
+    context.stroke();
+
+    Image
+    var img = document.getElementById("scream"); // ensure display:none on html
+    context.drawImage(img, 10, 10);
+
+    Circle
+    ellipse(cx, cy, rx, ry) {
+        context.save(); // save state
+        context.beginPath();
+        context.translate(cx, cy);
+        context.scale(rx/2, ry/2);
+        context.arc(1, 1, 1, 0, 2 * Math.PI, false);
+        context.fill();
+
+        context.restore(); // restore to original state
+    }
+   */
 
   done() {
     if(this.userService.isLoggedIn()) {
@@ -108,6 +228,34 @@ export class TileGameComponent implements OnInit {
     } else {
       console.log('done - not logged in');
     }
+  }
+
+  fixSizes() {
+    this.context.beginPath();
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    this.canvas.width = window.innerWidth - this.canvasOffsetX;
+    this.canvas.height = window.innerHeight - (this.canvasOffsetY * 2);
+    this.context.translate(0.5, 0.5);
+
+    this.gridOffsetX = this.canvas.width / 20;
+    this.gridOffsetY = this.canvas.height / 20;
+
+    var boardLength = Math.max(this.board.width, this.board.height);
+    var size = Math.min(this.canvas.offsetWidth - (this.gridOffsetX * 2), 
+                        this.canvas.offsetHeight - (this.gridOffsetY * 2));
+
+    let w = this.canvas.offsetWidth;
+    let h = this.canvas.offsetHeight;
+    if(w > h) {
+      this.gridOffsetX = Math.round( ( w - h) / 2 ) + this.gridOffsetX;
+    } else {
+      this.gridOffsetY = Math.round( (h - w) / 2 ) + this.gridOffsetY;
+    }
+
+    this.gridBoxSize = Math.round((size / boardLength));
+
+    this.draw();
   }
 
   /* EVENT LISTENERS */
@@ -128,9 +276,61 @@ export class TileGameComponent implements OnInit {
   }
 
   keyPressed(keyEvent) {
-    console.log({'keyPressed':keyEvent.keyCode});
+    //console.log({'keyPressed':keyEvent.keyCode});
+
+    let code = keyEvent.keyCode;
+    var directions = this.board.getValidDirections();
+    switch(code) {
+      // UP
+      case(40):
+      case(83):
+          if(directions.includes(0)) {
+            this.board.moveUp();
+            this.draw();
+            if(this.board.isSolved()) {
+              this.done();
+            }
+          }
+        break;
+
+      // DOWN
+      case(38):
+      case(87):
+          if(directions.includes(1)) {
+            this.board.moveDown();
+            this.draw();
+            if(this.board.isSolved()) {
+              this.done();
+            }
+          }
+        break;
+
+      // LEFT
+      case(39):
+      case(68):
+          if(directions.includes(2)) {
+            this.board.moveLeft();
+            this.draw();
+            if(this.board.isSolved()) {
+              this.done();
+            }
+          }
+        break;
+
+      // RIGHT
+      case(37):
+      case(65):
+          if(directions.includes(3)) {
+            this.board.moveRight();
+            this.draw();
+            if(this.board.isSolved()) {
+              this.done();
+            }
+          }
+        break;
+    }
   }
   keyReleased(keyEvent) {
-    console.log({'keyReleased':keyEvent.keyCode});
+    //console.log({'keyReleased':keyEvent.keyCode});
   }
 }
--- front-end/src/app/services/boards/tile-game/board.service.ts
+++ front-end/src/app/services/boards/tile-game/board.service.ts
@@ -1,8 +1,132 @@
 export class Board {
   seed: number;
-  constructor() { }
 
-  generateBoard() { }
+  width: number;
+  height: number;
+
+  // 2d array of numbers - 0 is the empty slot
+  tilePuzzle: number[][];
+
+  NUM_SWITCHES: number = 1000;
+
+  emptyX: number = 0;
+  emptyY: number = 0;
+
+  constructor(width, height, seed) { 
+    this.width = width;
+    this.height = height;
+    this.seed = seed;
+
+    this.emptyX = width - 1;
+    this.emptyY = height - 1;
+  }
+
+  generateBoard() { 
+    var rows = [];
+    for(var i = 0 ; i < this.height ; i++) {
+      var column = [];
+      for(var j = 0 ; j < this.width; j++) {
+        column.push(1 + j + ((i * this.width)));
+      }
+
+      rows.push(column);
+    }
+
+    this.tilePuzzle = rows;
+    this.tilePuzzle[this.height - 1][this.width - 1] = 0;
+
+    for(var _ = 0 ; _ < this.NUM_SWITCHES ; _++) {
+      this.switchTiles();
+    }
+  }
+
+  switchTiles() {
+    var validDirections = this.getValidDirections();
+    var dir = validDirections[Math.floor(this.random() * validDirections.length)];
+
+    switch(dir) {
+      // UP
+      case(0):
+          this.moveUp();
+        break;
+
+      // DOWN
+      case(1):
+          this.moveDown();
+        break;
+
+      // LEFT
+      case(2):
+          this.moveLeft();
+        break;
+
+      // RIGHT
+      case(3):
+          this.moveRight();
+        break;
+    }
+  }
+
+  getValidDirections() {
+    //                       U  D  L  R
+    //var validDirections = [0, 1, 2, 3];
+    var validDirections = [];
+
+    if(this.emptyY != 0) {
+      validDirections.push(0);
+    }
+
+    if(this.emptyY != this.height - 1) {
+      validDirections.push(1);
+    }
+
+    if(this.emptyX != 0) {
+      validDirections.push(2);
+    }
+
+    if(this.emptyX != this.width - 1) {
+      validDirections.push(3);
+    }
+
+    return validDirections;
+  }
+
+  moveUp() {
+    this.tilePuzzle[this.emptyY][this.emptyX] = this.tilePuzzle[this.emptyY - 1][this.emptyX];
+    this.tilePuzzle[this.emptyY - 1][this.emptyX] = 0;
+    this.emptyY--;
+  }
+
+  moveDown() {
+    this.tilePuzzle[this.emptyY][this.emptyX] = this.tilePuzzle[this.emptyY + 1][this.emptyX];
+    this.tilePuzzle[this.emptyY + 1][this.emptyX] = 0;
+    this.emptyY++;
+  }
+
+  moveLeft() {
+    this.tilePuzzle[this.emptyY][this.emptyX] = this.tilePuzzle[this.emptyY][this.emptyX - 1];
+    this.tilePuzzle[this.emptyY][this.emptyX - 1] = 0;
+    this.emptyX--;
+  }
+
+  moveRight() {
+    this.tilePuzzle[this.emptyY][this.emptyX] = this.tilePuzzle[this.emptyY][this.emptyX + 1];
+    this.tilePuzzle[this.emptyY][this.emptyX + 1] = 0;
+    this.emptyX++;
+  }
+
+  isSolved() { 
+    for(var j = 0 ; j < this.height ; j++) {
+      for(var i = 0 ; i < this.width ; i++) {
+        var val = this.tilePuzzle[j][i];
+        if(val != (i + 1) + (j * this.width) && val != 0) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
 
   random() {
       var x = Math.sin(++this.seed) * 10000;
