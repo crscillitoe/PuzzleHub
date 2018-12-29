35198319179d2078120b3ba71fdcc2a6a495390b
rkaldawy <remykaldawy@gmail.com>
2018-12-29 14:42:47 -0500

    rudimentary, unplayable minesweeper board

--- front-end/src/app/games/minesweeper/minesweeper.component.ts
+++ front-end/src/app/games/minesweeper/minesweeper.component.ts
@@ -23,8 +23,15 @@ export class MinesweeperComponent implements OnInit {
   canvasOffsetX: number = 225;
   canvasOffsetY: number = 56;
 
+  gridBoxSize: number = 20; //needs to be dynamically adjusted by fixed sizes
+  
+  gridOffsetX: number = 100;
+  gridOffsetY: number = 100;
+  
   difficulty: number;
   seed: number;
+
+  board: Board;
 
   constructor(
     private route: ActivatedRoute, 
@@ -42,24 +49,36 @@ export class MinesweeperComponent implements OnInit {
     this.canvas = document.getElementById('myCanvas');
     this.context = this.canvas.getContext('2d');
 
+    var width;
+    var height;
+    var bombCount;
+
     // Easy
     if(this.difficulty == 1) {
-      console.log('Easy difficulty');
+      width = 9;
+      height = 9;
+      bombCount = 10; //ratio of 8
     } 
     
     // Medium
     else if (this.difficulty == 2) {
-      console.log('Medium difficulty');
+      width = 16;
+      height = 16;
+      bombCount = 40; //ratio of 6.2
     } 
     
     // Hard
     else if (this.difficulty == 3) {
-      console.log('Hard difficulty');
+      width = 16;
+      height = 30;
+      bombCount = 99; //ratio of 4.8
     } 
     
     // Extreme
     else if (this.difficulty == 4) {
-      console.log('Extreme difficulty');
+      width = 30;
+      height = 30;
+      bombCount = 280; //ratio of 3.2
     }
 
     // Uncomment these to add event listeners
@@ -78,12 +97,29 @@ export class MinesweeperComponent implements OnInit {
           // Generate board with given seed
           this.seed = data['seed'];
 
+          this.board = new Board(width, height, bombCount, this.seed);
+          this.board.generateBoard();
+          
+          this.canvas.addEventListener('mouseup',   (e) => this.mouseReleased(e), false); 
+          this.canvas.addEventListener('mousedown', (e) => this.mousePressed(e),  false);
+          //this.displayTimer();
+
+          this.fixSizes();
           this.draw();
         });
     } else {
       // Generate board with random seed
       this.seed = Math.floor(Math.random() * (2000000000));
+      
+      this.board = new Board(width, height, bombCount, this.seed);
+      this.board.generateBoard();
+ 
+      this.canvas.addEventListener('mouseup',   (e) => this.mouseReleased(e), false); 
+      this.canvas.addEventListener('mousedown', (e) => this.mousePressed(e),  false);
+      
+      //this.displayTimer();
 
+      this.fixSizes();
       this.draw();
     }
   }
@@ -91,11 +127,62 @@ export class MinesweeperComponent implements OnInit {
   draw() {
     this.context.beginPath();
     this.drawBackground();
+    this.drawGrid();
+    this.drawTiles();
   }
 
   drawBackground() {
     this.context.fillStyle = this.colors.BACKGROUND; // Background color
     this.context.fillRect(0, 0, this.canvas.offsetWidth * 2, this.canvas.offsetHeight * 2);
+  }
+
+  drawGrid() {
+    for(var i = 0; i <= this.board.width; i++) {
+      this.context.lineWidth = 1;
+      this.context.strokeStyle = this.colors.FOREGROUND;
+      this.context.moveTo(this.gridOffsetX + (i * this.gridBoxSize), this.gridOffsetY);
+      this.context.lineTo(this.gridOffsetX + (i* this.gridBoxSize), this.gridOffsetY + (this.board.height * this.gridBoxSize));
+      this.context.stroke();
+    }
+
+    for(var j = 0; j <= this.board.height; j++) {
+      this.context.lineWidth = 1;
+      this.context.strokeStyle = this.colors.FOREGROUND;
+      this.context.moveTo(this.gridOffsetX, this.gridOffsetY + (j * this.gridBoxSize));
+      this.context.lineTo(this.gridOffsetX + (this.board.width * this.gridBoxSize), this.gridOffsetY + (j * this.gridBoxSize));
+      this.context.stroke();
+    }
+  }
+
+  drawTiles() {
+    for(var j = 0; j < this.board.height; j++) {
+      for(var i = 0; i < this.board.width; i++) {
+
+        var boardValue = this.board.mineField[j][i];
+        var tileString
+
+        if(boardValue == 0) tileString = ''
+        else if(boardValue == -1) tileString = 'B';
+        else tileString = "" + boardValue;
+
+        this.context.font = 'Bold ' + Math.floor(this.gridBoxSize / 1.6) + 'px Poppins';
+        this.context.textAlign = "center";
+        this.context.fillStyle = this.colors.COLOR_1;
+
+        if(tileString == "1") this.context.fillStyle = this.colors.COLOR_2;
+        if(tileString == "2") this.context.fillStyle = this.colors.COLOR_1;
+        if(tileString == "3") this.context.fillStyle = this.colors.COLOR_3;
+        if(tileString == "4") this.context.fillStyle = this.colors.COLOR_4;
+        if(tileString == "5") this.context.fillStyle = this.colors.COLOR_5;
+        if(tileString == "6") this.context.fillStyle = this.colors.COLOR_6;
+				if(tileString == "7") this.context.fillStyle = this.colors.COLOR_7;
+        if(tileString == "8") this.context.fillStyle = this.colors.COLOR_8
+        
+        this.context.fillText(tileString, (this.gridOffsetX) + ( i * this.gridBoxSize ) + (this.gridBoxSize / 2),
+                                          (this.gridOffsetY) + ( (j + 1) * this.gridBoxSize ) - (this.gridBoxSize / 4)); 
+      }
+    }
+
   }
 
   done() {
@@ -107,6 +194,34 @@ export class MinesweeperComponent implements OnInit {
     } else {
       console.log('done - not logged in');
     }
+  }
+
+  fixSizes() {
+    this.context.beginPath();
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    this.canvas.width = window.innerWidth - this.canvasOffsetX;
+    this.canvas.height = window.innerHeight - (this.canvasOffsetY * 2);
+    this.context.translate(0.5, 0.5);
+
+    this.gridOffsetX = this.canvas.width / 20;
+    this.gridOffsetY = this.canvas.height / 20;
+
+    var boardLength = Math.max(this.board.width, this.board.height);
+    var size = Math.min(this.canvas.offsetWidth - (this.gridOffsetX * 2), 
+                        this.canvas.offsetHeight - (this.gridOffsetY * 2));
+
+    let w = this.canvas.offsetWidth;
+    let h = this.canvas.offsetHeight;
+    if(w > h) {
+      this.gridOffsetX = Math.round( (w - h) / 2 ) + this.gridOffsetX;
+    } else {
+      this.gridOffsetY = Math.round( (h - w) / 2 ) + this.gridOffsetY;
+    }
+
+    this.gridBoxSize = Math.round((size / boardLength));
+
+    this.draw();
   }
 
   /* EVENT LISTENERS */
--- front-end/src/app/services/boards/minesweeper/board.service.ts
+++ front-end/src/app/services/boards/minesweeper/board.service.ts
@@ -1,8 +1,91 @@
 export class Board {
   seed: number;
-  constructor() { }
 
-  generateBoard() { }
+  width: number;
+  height: number;
+  bombCount: number;
+
+  mineField: number[][];
+
+  constructor(width, height, bombCount, seed) {
+    this.width = width;
+    this.height = height;
+    //the silent "first bomb" gets removed so you cant insta-lose
+    this.bombCount = bombCount + 1;
+    this.seed = seed;
+  }
+
+  generateBoard() { 
+    var rows = [];
+    for(var j  = 0; j < this.height; j++) {
+      var column = [];
+      for(var i = 0; i < this.width; i++) {
+        column.push(0);
+      }
+
+      rows.push(column);
+    }
+
+    this.mineField = rows;
+
+    var mineXPos;
+    var mineYPos;
+
+    for(var _ = 0; _ < this.bombCount; _++) {
+      do {
+        mineXPos = Math.floor(this.random() * this.width);
+        mineYPos = Math.floor(this.random()* this.height);
+      } while (!this.checkValidMine(mineXPos, mineYPos));    
+
+      for(var i = -1; i <= 1; i++){
+        for(var j = -1; j <= 1; j++){
+          if(i == 0 && j == 0){
+            this.mineField[mineYPos][mineXPos] = -1;
+          }
+          else{
+            this.updateNeighborTile(mineXPos+i, mineYPos+j);
+          }
+        }
+      }
+    }
+
+    console.log(this.mineField);
+  }
+
+  checkValidMine(x, y){
+
+    if(this.mineField[y][x] < 0){
+      return false;
+    }
+
+    var nbrMineCount = 0;
+    //at least two free spaces must surround a mine
+    for(var i = x-1; i <= x+1; i++){
+      for(var j = y-1; j <= y+1; j++){
+        if(i >= 0 && i < this.width && j >= 0 && j < this.height){
+          if(this.mineField[j][i] < 0){
+            nbrMineCount += 1;
+          }
+        } 
+      }
+    
+    }
+    if(nbrMineCount >= 2){
+      return false;
+    }
+
+    return true;
+    
+    //TODO: check for overdistant mines
+  }
+
+  updateNeighborTile(x, y){
+    if(x >=0 && x < this.width && y >= 0 && y < this.height){
+      if(this.mineField[y][x] >= 0){
+        this.mineField[y][x] += 1;
+      }
+    }
+  }
 
   random() {
       var x = Math.sin(++this.seed) * 10000;
