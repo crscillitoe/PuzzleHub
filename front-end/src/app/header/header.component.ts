33124799cc730ecbbd836977fa0e0152683fb462
crscillitoe <crscillitoe@wpi.edu>
2019-02-05 21:05:36 -0500

    changes for electron app

--- front-end/src/app/header/header.component.ts
+++ front-end/src/app/header/header.component.ts
@@ -41,4 +41,29 @@ export class HeaderComponent implements OnInit {
   ngOnInit() {
   }
 
+  isElectron() {
+    // Renderer process
+    if (typeof window !== 'undefined' && typeof window['process'] === 'object' && window['process'].type === 'renderer') {
+        return true;
+    }
+
+    // Main process
+    if (typeof window['process'] !== 'undefined' && typeof window['process'].versions === 'object' && !!window['process'].versions.electron) {
+        return true;
+    }
+
+    // Detect the user agent when the `nodeIntegration` option is set to true
+    if (typeof navigator === 'object' && typeof navigator.userAgent === 'string' && navigator.userAgent.indexOf('Electron') >= 0) {
+        return true;
+    }
+
+    return false;
+  }
+
+  close() {
+    window.close();
+  }
+
+  maximize() {
+  }
 }
