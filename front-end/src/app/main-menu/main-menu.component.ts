2b1d1f613571bac78d8ccb432d096b6643d61c62
Graham Lupton <grahamlup@gmail.com>
2019-04-04 00:28:44 +0000

    Resolves #21.

--- front-end/src/app/main-menu/main-menu.component.ts
+++ front-end/src/app/main-menu/main-menu.component.ts
@@ -1,20 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { TimerService } from '../services/timer/timer.service';
 import { TunnelService } from '../services/tunnel/tunnel.service';
 import { LoaderService } from '../services/loading-service/loader.service';
 import { UserService } from '../services/user/user.service';
 import { Router } from '@angular/router';
 import { GameListAllService } from '../services/games/game-list-all.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-main-menu',
   templateUrl: './main-menu.component.html',
   styleUrls: ['./main-menu.component.scss']
 })
-export class MainMenuComponent implements OnInit {
+export class MainMenuComponent implements OnInit, OnDestroy {
 
   games: any = GameListAllService.games;
   level = 0;
+  subscription: Subscription;
 
   constructor(
     private timerService: TimerService,
@@ -23,6 +25,14 @@ export class MainMenuComponent implements OnInit {
     private user: UserService,
     private loader: LoaderService
   ) { }
+
+  ngOnInit() {
+    this.level = this.getLevel();
+    this.subscription = this.user.level
+      .subscribe( (data) => {
+        this.level = data;
+      });
+  }
 
   isLoggedIn() {
     return this.user.isLoggedIn();
@@ -40,11 +50,7 @@ export class MainMenuComponent implements OnInit {
     this.router.navigate([route, m]);
   }
 
-  ngOnInit() {
-    this.user.level
-      .subscribe( (data) => {
-        this.level = data;
-        console.log(this.level + ' ' + data);
-      });
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
