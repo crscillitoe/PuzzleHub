236edda4a6a5aa3a862665cc6c0b25a178e3fc2f
crscillitoe <crscillitoe@wpi.edu>
2019-05-02 22:02:20 -0400

    get footer endpoint added to leaderboards

--- api/leaderboards.py
+++ api/leaderboards.py
@@ -726,3 +726,98 @@ def get_leaderboards_siege():
             to_return.append(model)
 
     return jsonify(to_return)
+
+# ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
+# /getFooter
+# Required POST parameters:
+#   GameID: int
+#   Difficulty: int
+#                     D   W   M
+#   Leaderboard: int (0 - 1 - 2)
+# Returns on failure:
+#   No return, throw error on failure.
+@app.route('/api/getFooter', methods=['POST'])
+@cross_origin(supports_credentials=True)
+def get_footer():
+    try:
+        user_id = get_user_id(xstr(request.headers.get('PuzzleHubToken')))
+    except:
+        return jsonify([])
+
+    try:
+        game_id = request.json["GameID"]
+    except:
+        abort(500, "GameID not found")
+
+    try:
+        difficulty = request.json["Difficulty"]
+    except:
+        abort(500, "Difficulty not found")
+
+    try:
+        leaderboard = request.json["Leaderboard"]
+    except:
+        abort(500, "Leaderboard not found")
+
+    cursor = db.cursor()
+    if leaderboard == 0:
+        sql_query = '''
+            SELECT Username, TimeElapsed, Role, BronzeMedals, SilverMedals, GoldMedals
+            FROM dailyLeaderboards AS L
+            INNER JOIN users AS U ON
+            U.UserID = L.UserID
+            INNER JOIN userMedals AS UM ON
+            UM.UserID = L.UserID AND
+            UM.Type = 0
+            WHERE GameID = %(game_id)s AND Difficulty = %(difficulty)s
+                AND U.UserID = %(user_id)s
+        '''
+    elif leaderboard == 1:
+        sql_query = '''
+            SELECT Username, TimeElapsed, Role, BronzeMedals, SilverMedals, GoldMedals
+            FROM weeklyLeaderboards AS L
+            INNER JOIN users AS U ON
+            U.UserID = L.UserID
+            INNER JOIN userMedals AS UM ON
+            UM.UserID = L.UserID AND
+            UM.Type = 1
+            WHERE GameID = %(game_id)s AND Difficulty = %(difficulty)s
+                AND U.UserID = %(user_id)s
+        '''
+    elif leaderboard == 2:
+        sql_query = '''
+            SELECT Username, TimeElapsed, Role, BronzeMedals, SilverMedals, GoldMedals
+            FROM monthlyLeaderboards AS L
+            INNER JOIN users AS U ON
+            U.UserID = L.UserID
+            INNER JOIN userMedals AS UM ON
+            UM.UserID = L.UserID AND
+            UM.Type = 2
+            WHERE GameID = %(game_id)s AND Difficulty = %(difficulty)s
+                AND U.UserID = %(user_id)s
+        '''
+
+    query_model = {
+        "game_id": game_id,
+        "difficulty": difficulty,
+        "user_id": user_id
+    }
+
+    cursor.execute(sql_query, query_model)
+    data = cursor.fetchall()
+
+    to_return = []
+
+    for d in data:
+        model = {
+            "username":d[0],
+            "time":convert_to_puzzle_hub_date(d[1]),
+            "role":str(d[2]),
+            "bronzeMedals":d[3],
+            "silverMedals":d[4],
+            "goldMedals":d[5],
+            "position":0
+        }
+        to_return.append(model)
+
+    return jsonify(to_return)
