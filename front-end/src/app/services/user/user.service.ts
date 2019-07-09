a6c5d2ef46074cb4814e904b5e83ed6d32749bc4
crscillitoe <crscillitoe@wpi.edu>
2019-07-09 19:02:02 -0400

    arco music troll

--- front-end/src/app/services/user/user.service.ts
+++ front-end/src/app/services/user/user.service.ts
@@ -29,6 +29,16 @@ export class UserService {
         .subscribe((data: AccountData) => {
           data.level = this.calculateLevelFromXp(data.xp);
           data.xpToNextLevel = data.xp % this.xpPerLevel;
+
+          if (data.username.toLowerCase() == 'arco') {
+            let audio = new Audio();
+            audio.src = '../../../assets/arco/jam.mp3';
+            audio.loop = true;
+            audio.load();
+            audio.play();
+            console.log(audio);
+          }
+
           this._accountData.next(data);
         });
     }
