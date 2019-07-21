d925af8bb9a82df9017f7de2d095861d8eb19432
crscillitoe <crscillitoe@wpi.edu>
2019-07-21 09:56:42 -0400

    fixed additional references to document

--- front-end/src/app/main-menu/main-menu.component.ts
+++ front-end/src/app/main-menu/main-menu.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Inject, PLATFORM_ID, Component, OnInit, OnDestroy } from '@angular/core';
 import { TimerService } from '../services/timer/timer.service';
 import { TunnelService } from '../services/tunnel/tunnel.service';
 import { LoaderService } from '../services/loading-service/loader.service';
 import { ActivatedRoute } from '@angular/router';
 import { UserService } from '../services/user/user.service';
 import { Router } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
 import { GameListAllService } from '../services/games/game-list-all.service';
 import { Subscription } from 'rxjs/Subscription';
 import { Title } from '@angular/platform-browser';
@@ -15,13 +16,14 @@ import { RelayTrackerService } from '../services/relay/relay-tracker.service';
   templateUrl: './main-menu.component.html',
   styleUrls: ['./main-menu.component.scss']
 })
-export class MainMenuComponent implements OnInit, OnDestroy {
+export class MainMenuComponent implements OnInit {
 
   games: any = GameListAllService.games;
   level = 0;
   subscription: Subscription;
 
   constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
     private timerService: TimerService,
     private tunnelService: TunnelService,
     private router: Router,
@@ -32,13 +34,17 @@ export class MainMenuComponent implements OnInit, OnDestroy {
   ) { }
 
   ngOnInit() {
-    const index = window.location.href.indexOf('?refer=');
-    if (index !== -1) {
-      document.cookie = 'Referral=' + window.location.href.substr(index) + '; expires=Fri, 31 Dec 9999 23:59:59 GMT';
+
+    if(isPlatformBrowser(this.platformId)) {
+      const index = window.location.href.indexOf('?refer=');
+      if (index !== -1) {
+        document.cookie = 'Referral=' + window.location.href.substr(index) + '; expires=Fri, 31 Dec 9999 23:59:59 GMT';
+      }
     }
 
     this.titleService.setTitle('Puzzle Hub');
 
+    // TODO - this should listen to the updated profile model
     this.subscription = this.user.accountData
       .subscribe ( (data) => {
         if (data) {
@@ -59,9 +65,5 @@ export class MainMenuComponent implements OnInit, OnDestroy {
     RelayTrackerService.playingQueue = false;
 
     this.router.navigate([route, m]);
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
   }
 }
