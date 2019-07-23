09e5b2504fc42fa55bde53f3fff6d5630cf9650e
crscillitoe <crscillitoe@wpi.edu>
2019-07-23 17:27:48 -0400

    xp bar component

---
+++ front-end/src/app/xp-bar/xp-bar.component.ts
@@ -0,0 +1,55 @@
+import { PLATFORM_ID, Inject, Input, Component, OnInit, OnChanges } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+
+@Component({
+  selector: 'app-xp-bar',
+  templateUrl: './xp-bar.component.html',
+  styleUrls: ['./xp-bar.component.scss']
+})
+export class XpBarComponent implements OnInit, OnChanges {
+
+  @Input() progress: number;
+  @Input() color: string;
+  @Input() accentColor: string;
+  @Input() className: string;
+
+  private styleElement: HTMLStyleElement = null;
+
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+  }
+
+  ngOnInit() {
+    this.styleElement = document.createElement('style');
+    this.updateColors();
+  }
+
+  ngOnChanges() {
+    this.updateColors();
+  }
+
+  updateColors() {
+    if (isPlatformBrowser(this.platformId)) {
+      try {
+        const head = document.getElementsByTagName('head')[0];
+        const css = `
+          #${this.className} .mat-progress-bar-fill::after {
+            background-color: ${this.color} !important;
+          }
+          #${this.className} .mat-progress-bar-buffer {
+            background-color: ${this.accentColor} !important;
+          }
+        `
+
+        this.styleElement.innerHTML = '';
+        this.styleElement.type = 'text/css';
+        this.styleElement.appendChild(document.createTextNode(css));
+        head.appendChild(this.styleElement);
+      } catch (e) {
+
+      }
+    }
+  }
+
+}
