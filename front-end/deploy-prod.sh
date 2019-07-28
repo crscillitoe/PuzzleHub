32068ffc07c8bbe66d23b19729020c9a885a47cb
crscillitoe <crscillitoe@wpi.edu>
2019-07-28 11:37:11 -0400

    update deploy prod for permanent uptime, and nginx config

--- front-end/deploy-prod.sh
+++ front-end/deploy-prod.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-set -x
 
 BUILD_PATH="."
 BUILD_DIR="dist"
@@ -13,10 +12,18 @@ npm run build:ssr
 zip -r $BUILD_DIR.zip $BUILD_PATH/$BUILD_DIR
 scp -i $PUZZLEHUB_KEY $BUILD_DIR.zip $DEPLOY_USER@$DEPLOY_IP:$DEPLOY_DIR
 ssh -i $PUZZLEHUB_KEY $DEPLOY_USER@$DEPLOY_IP << EOF
+set -x
 cd SSR
-forever stopall
-cp -r dist backup_dist
-rm -rf dist
+rm -rf backup_dist
+forever stop server_1/server.js
+cp -r server_1 backup_dist
+rm -rf server_1
 unzip dist.zip
-PORT=1234 forever start dist/server.js
+cp -r dist server_1
+PORT=1234 forever start server_1/server.js
+forever stop server_2/server.js
+rm -rf server_2
+cp -r dist server_2
+PORT=1235 forever start server_2/server.js
+rm -rf dist
 EOF
