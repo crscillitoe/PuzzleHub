bd97c89dffd7b6c5c306b00554eba009840d8804
Bradford Bonanno <bwbonanno@wpi.edu>
2019-07-29 21:44:49 -0400

    Revert prod script

--- front-end/src/app/services/icons/icon.service.ts
+++ front-end/src/app/services/icons/icon.service.ts
@@ -46,7 +46,7 @@ export class IconService {
   }
 
   getIconImagePath(iconID: number) {
-    if (!this.hasData) throw "Error: IconData has not loaded";
+    if (!this.hasData) 'puzzle-hub-profile-0.png';
     return this.getIcon(iconID).IconPath;
   }
 
